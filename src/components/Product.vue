727ac5464291ae478eacc94b3f0cea49a74f9319
Chani.Mishkovsky <chanistern123@gmail.com>
2021-01-10 03:45:26 +0200

    add products

---
+++ src/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product-card">
+    <div class="product-image">
+      <img :src="product.image" :alt="product.title" />
+    </div>
+    <div class="product-info">
+      <h5>{{ product.title }}</h5>
+      <h6>{{ product.price }}</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,85 @@
+<template>
+  <nav class="product-filter">
+    <div class="sort">
+      <div class="collection-sort">
+        <label>Filter by:</label>
+        <select v-model="selectedCategory">
+          <option
+            v-for="(category, index) in categories"
+            :key="index"
+            :value="category"
+          >
+            {{ category }}
+          </option>
+        </select>
+      </div>
+
+      <div class="collection-sort">
+        <label>Sort by:</label>
+        <select>
+          <option value="/">Featured</option>
+          <option value="/">Best Selling</option>
+          <option value="/">Alphabetically, A-Z</option>
+          <option value="/">Alphabetically, Z-A</option>
+          <option value="/">Price, low to high</option>
+          <option value="/">Price, high to low</option>
+          <option value="/">Date, new to old</option>
+          <option value="/">Date, old to new</option>
+        </select>
+      </div>
+    </div>
+  </nav>
+
+  <section class="products">
+    <Product
+      v-for="product in filteredProducts"
+      :key="product.id"
+      :product="product"
+    >
+    </Product>
+  </section>
+</template>
+
+<script>
+import Product from "./Product";
+import axios from "axios";
+import { groupBy } from "@/composables/utils.js";
+export default {
+  components: { Product },
+  data() {
+    return {
+      selectedCategory: "--Please choose an option--",
+      products: [],
+    };
+  },
+  async mounted() {
+    try {
+      this.products = (
+        await axios.get("https://fakestoreapi.com/products")
+      ).data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  computed: {
+    categories() {
+      return [
+        "--Please choose an option--",
+        ...Object.keys(groupBy(this.products, "category")),
+      ];
+    },
+    filteredProducts() {
+      if (this.selectedCategory === "--Please choose an option--")
+        return this.products;
+      else {
+        return this.products.filter(
+          (product) => product.category === this.selectedCategory
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
