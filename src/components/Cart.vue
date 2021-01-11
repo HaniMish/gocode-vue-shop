ddc9c32b9691602026ed27dbf839392f8778d4c5
Chani.Mishkovsky <chanistern123@gmail.com>
2021-01-12 00:27:48 +0200

    ex6. add cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="cart">Cart Quantity: {{count}}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        count: {
+            type: Number,
+            default: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
